71597796b95c39ea4d433a823a72151cfb175e2a
tavjacobs-png <tavjacobs@gmail.com>
2026-09-17 16:59:44 +0100

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'Slot QA Release Hub',description:'Game release and QA control centre'};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
